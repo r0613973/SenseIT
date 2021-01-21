0501ad0d18174f2071d5c029f128f598818b40ba
Matthiasvdpoel <r0667263@student.thomasmore.be>
2021-01-21 10:57:07 +0100

    werkend gekregen matthias

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<h1>Contact info</h1>
+
+<p>SenseIT</p>
+<p><a href="mailto:info@thevinylshop.com">info@senseIT.com</a></p>
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" />
+    <title>@yield('title', 'The Vinyl Shop')</title>
+</head>
+<body>
+    @include('shared.navigation')
+<main class="container mt-3">
+<main class="container">
+    @yield('main', 'Page under construction ...')
+</main>
+{{--  Footer  --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.js"></script>
+</body>
+</html>
