0c3dfb2a8ed0eba9664b32a7fdb872a3172a4f55
rubenlie <36152264+rubenlie@users.noreply.github.com>
2021-02-04 10:33:29 +0100

    toggle map

--- resources/views/data-schermen/sateliet.blade.php
+++ resources/views/data-schermen/sateliet.blade.php
@@ -34,45 +34,7 @@
     <script>
         const MDCList= mdc.list.MDCList;
         new MDCList(document.querySelector('.mdc-list'));
-        $('document').ready(function(){
 
-
-        // enumerate ids of the layers
-        var toggleableLayerIds = ['contours', 'museums'];
-
-        // set up the corresponding toggle button for each layer
-        for (var i = 0; i < toggleableLayerIds.length; i++) {
-            var id = toggleableLayerIds[i];
-
-            var link = document.createElement('a');
-            link.href = '#';
-            link.className = 'active';
-            link.textContent = id;
-
-            link.onclick = function (e) {
-                var clickedLayer = this.textContent;
-                e.preventDefault();
-                e.stopPropagation();
-
-                var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-
-// toggle layer visibility by changing the layout object's visibility property
-                if (visibility === 'visible') {
-                    map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-                    this.className = '';
-                } else {
-                    this.className = 'active';
-                    map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-                }
-            };
-
-            var layers = document.getElementById('menu');
-            layers.appendChild(link);
-        }
-        });
-        map.on('load', function () {
-            map.resize();
-        });
     </script>
     <script>
         const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
